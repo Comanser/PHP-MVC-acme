b163d9521f464f3121f1b1beb38325559cbb7a00
Mario Misiuna <comanser@wp.pl>
2016-04-18 22:52:37 +0200

    Development
    Added database migrations; updated composer.json to use phinx; added
    phinx files

--- routes.php
+++ routes.php
@@ -2,12 +2,19 @@
 
 $router->map('GET', '/', 'Acme\Controllers\PageController@getShowHomePage', 'home');
 
+$router->map('GET', '/about', 'Acme\Controllers\PageController@getShowPage', 'generic_page');
+
 $router->map('GET', '/register', 'Acme\Controllers\RegisterController@getShowRegisterPage', 'register');
 
 $router->map('POST', '/register', 'Acme\Controllers\RegisterController@postShowRegisterPage', 'register_post');
 
 $router->map('GET', '/login', 'Acme\Controllers\RegisterController@getShowLoginPage', 'login');
 
+
+
+
+
+// Educational purpose
 $router->map('GET', '/yellow', 'Acme\Controllers\PageController@getShowLoginPage', 'yellow');
 
 $router->map('GET', '/testdb', 'Acme\Controllers\RegisterController@getTestDB', 'testdb');
--- src/controllers/PageController.php
+++ src/controllers/PageController.php
@@ -6,8 +6,11 @@ use duncan3dc\Laravel\BladeInstance;
 class PageController extends BaseController
 {
   public function getShowHomePage() {
-    //echo $this->twig->render('home.html');
     //$_SESSION['test'] = "<strong>I hope this works!</strong>";
     echo $this->blade->render("home", ['test' => 'Hello, again.']);
+  }
+
+  public function getShowPage() {
+    echo "foo";
   }
 }
